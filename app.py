9971a2ae04c64a1dc93544bb266ae4d950cacafc
Jeanne <jeannefernanda.m@gmail.com>
2023-08-17 11:18:07 -0400

    Configurando a conexão com o banco de dados MySQL

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import mysql.connector
 
 app = Flask(__name__)
 
+# Configuração do banco de dados
+db_config = {
+    'user': 'root',
+    'password': 'root',
+    'host': 'localhost',
+    'database': 'escola',
+    'ssl_disabled': True
+}
+
 @app.route('/')
 def index():
     return render_template('index.html')

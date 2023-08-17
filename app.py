1c2725955f2d1ad687c1848dd36d94af0bbf8e95
Jeanne <jeannefernanda.m@gmail.com>
2023-08-17 11:15:06 -0400

    Pagina inicial criada

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
